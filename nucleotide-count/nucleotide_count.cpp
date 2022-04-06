e26c3f521edba2bcf5125f9af07dfca9e1cf750a
aabuelazm <abdot525@gmail.com>
2022-04-06 14:19:43 -0700

    First Test

--- nucleotide-count/nucleotide_count.cpp
+++ nucleotide-count/nucleotide_count.cpp
@@ -1,5 +1,31 @@
 #include "nucleotide_count.h"
 
-namespace nucleotide_count {
+using namespace std;
 
-}  // namespace nucleotide_count
+namespace nucleotide_count {
+counter::counter(std::string dna_sequence) {
+  const auto first = begin(valid_nucleotides);
+  const auto last = end(valid_nucleotides);
+
+  for (auto c : dna_sequence) {
+    if (find(first, last, c) != last) {
+      this->nucleotides.at(c)++;
+    } else
+      throw invalid_argument("Nucleotides can only be A, T, C, or G.");
+  }
+}
+
+std::map<char, int> counter::nucleotide_counts() const {
+  return this->nucleotides;
+}
+
+unsigned int counter::count(char nucleotide) const {
+  const auto first = begin(valid_nucleotides);
+  const auto last = end(valid_nucleotides);
+
+  if (find(first, last, nucleotide) == last)
+    throw invalid_argument("Nucleotides can only be A, T, C, or G.");
+
+  return this->nucleotides.at(nucleotide);
+}
+} // namespace nucleotide_count
--- nucleotide-count/nucleotide_count.h
+++ nucleotide-count/nucleotide_count.h
@@ -1,6 +1,7 @@
 #if !defined(NUCLEOTIDE_COUNT_H)
 #define NUCLEOTIDE_COUNT_H
 
+#include <algorithm>
 #include <exception>
 #include <map>
 #include <stdexcept>
@@ -10,11 +11,12 @@ namespace nucleotide_count {
 class counter {
 private:
   std::map<char, int> nucleotides = {{'A', 0}, {'T', 0}, {'C', 0}, {'G', 0}};
+  const char valid_nucleotides[4] = {'A', 'T', 'C', 'G'};
 
 public:
   counter(std::string dna_sequence);
-  std::map<char, int> nucleotide_counts();
-  unsigned int count(char nucleotide);
+  std::map<char, int> nucleotide_counts() const;
+  unsigned int count(char nucleotide) const;
 };
 } // namespace nucleotide_count
 
