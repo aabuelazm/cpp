02290ac59c73dcf938c1f7cf2df896b8df9f6752
aabuelazm <abdot525@gmail.com>
2022-04-10 11:03:29 -0700

    Did math, it worked

--- difference-of-squares/difference_of_squares.cpp
+++ difference-of-squares/difference_of_squares.cpp
@@ -1,5 +1,20 @@
 #include "difference_of_squares.h"
 
+using namespace std;
+
 namespace difference_of_squares {
 
-}  // namespace difference_of_squares
+unsigned long long square_of_sum(unsigned long long input) {
+  auto sum = ((input + 1) * input) / 2;
+  return sum * sum;
+}
+
+unsigned long long sum_of_squares(unsigned long long input) {
+  return (input * (input + 1) * ((2 * input) + 1)) / 6;
+}
+
+long long difference(unsigned long long input) {
+  return square_of_sum(input) - sum_of_squares(input);
+}
+
+} // namespace difference_of_squares
--- difference-of-squares/difference_of_squares.h
+++ difference-of-squares/difference_of_squares.h
@@ -1,8 +1,14 @@
 #if !defined(DIFFERENCE_OF_SQUARES_H)
 #define DIFFERENCE_OF_SQUARES_H
 
+#include <cmath>
+
 namespace difference_of_squares {
 
-}  // namespace difference_of_squares
+unsigned long long square_of_sum(unsigned long long input);
+unsigned long long sum_of_squares(unsigned long long input);
+long long difference(unsigned long long input);
+
+} // namespace difference_of_squares
 
 #endif // DIFFERENCE_OF_SQUARES_H
